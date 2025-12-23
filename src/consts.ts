08aa4da0bead3827fa85dfb0162f185e7784b0a0
natane <mimikaumi@gmail.com>
2025-12-23 11:40:48 +0900

    Initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ナタネの独り言';
+export const SITE_DESCRIPTION = '技術的なことや日々の気づきを記録するブログです。';
